b9780e7c070fcfad1f81637d28afb910045a0f7b
Dev Patel <104315781+DevpEth@users.noreply.github.com>
2023-01-29 03:23:49 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var links = document.querySelectorAll(".menu ul li a");
+
+for(var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function() {
+    var current = document.querySelector(".menu ul li a.active");
+    current.classList.remove("active");
+    this.classList.add("active");
+  });
+}
+
+var links = document.querySelectorAll('.left-menu a');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            window.open(this.href, '_blank');
+        });
+    });
+
+    if(performance.navigation.type == 1){
+        window.location.href = "#aboutme";
+    }
+
+
